d015d4950d69bf3801d011d9a65809b219e975bf
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-03-25 05:52:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# GitHub CircleCI Trigger
+
+A simple GitHub post-receive web hook handler able to trigger a CircleCI build.
+
+# motivation
+
+It is particularly useful for triggering CircleCI build using GitHub token.
+
+# quickstart
+
+First, from a terminal, install and start the server
+
+```bash
+# create virtualenv
+$ mkvirtualenvwrapper github-circleci-trigger
+
+# choose CircleCI project to trigger
+$ export CIRCLECI_REPO=orgname/worker-with-privileges
+$ export CIRCLECI_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# install server
+$ git clone https://github.com/Slicer/github-circleci-trigger
+$ pip install -r github-circleci-trigger/requirements
+
+# start server
+$ cd github-circleci-trigger
+$ FLASK_APP="github-circleci-trigger.py"
+$ GITHUB_WEBHOOK_SECRET="This is a secret"
+$ FLASK_DEBUG="1"
+$ python -m flask run
+ * Serving Flask app "github-circleci-trigger"
+ * Forcing debug mode on
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+```
+
+Then, open the URL ``http://127.0.0.1:5000`` in your favorite browser. It should display 
+``Hello World!`` on the page and report the following in your terminal:
+
+```
+127.0.0.1 - - [25/Mar/2017 04:50:48] "GET / HTTP/1.1" 200 -
+```
+
+Now, let's install [ngrok](https://ngrok.com/download) so that we can easily get a public URL
+and test.
+
+```bash
+$ unzip /path/to/ngrok.zip
+$ ngrok
+
+https://ff9dc197.ngrok.io -> localhost:5000
+```
+
+Open again the ``*.ngrok.io`` URL in your browser and it should still display ``Hello World!``.
+
+Last, open the settings of ``orgname/open-source-project`` GitHub project and add a webhook:
+
+ * Payload URL: ``https://ff9dc197.ngrok.io/postreceive``
+ * Content type: ``application/json``
+ * Secret: ``This is a secret``
+ * Let me select individual events: Check ``Push`` and ``Pull Request``
+
+Et voila, each time a commit is pushed (or a pull request is created) on ``orgname/open-source-project`` 
+our server will handle the GitHub webhook and trigger CircleCI build of ``orgname/worker-with-privileges``
+project with the following parameters:
+
+* ``SLICER_REPO_NAME``
+* ``SLICER_REPO_BRANCH``
+* ``SLICER_REPO_TAG``
+* ``SLICER_REPO_REVISION``
+
+# thanks
+
+This work was inspired from there projects:
+* https://github.com/razius/github-webhook-handler
+* https://github.com/carlos-jenkins/python-github-webhooks
+
+# license
+
+It is covered by the Slicer License:
+
+https://github.com/slicer-apidocs-builder/License.txt
---
+++ github-circleci-trigger.py
@@ -0,0 +1,147 @@
+
+import os
+
+from functools import wraps
+
+import requests
+
+from flask import abort, Flask, request
+from github_webhook import Webhook
+from ipaddress import ip_address, ip_network
+
+app = Flask(__name__)
+webhook = Webhook(app, endpoint='/postreceive', secret=os.environ.get('GITHUB_WEBHOOK_SECRET'))
+
+
+def ip_check(func):
+    @wraps(func)
+    def with_ip_check(*args, **kwargs):
+        # Store the IP address of the requester
+        request_ip = ip_address(u'{0}'.format(request.remote_addr))
+
+        # If GHE_ADDRESS is specified, use it as the hook_blocks.
+        if os.environ.get('GHE_ADDRESS', None):
+            hook_blocks = [unicode(os.environ.get('GHE_ADDRESS'))]
+        # Otherwise get the hook address blocks from the API.
+        else:
+            hook_blocks = requests.get('https://api.github.com/meta').json()['hooks']
+
+        # Support debugging
+        if os.environ.get('FLASK_DEBUG', 0):
+            hook_blocks.append(u'127.0.0.0/8')
+
+        # Check if the POST request is from github.com or GHE
+        for block in hook_blocks:
+            if ip_address(request_ip) in ip_network(block):
+                break  # the remote_addr is within the network range of github.
+        else:
+            abort(403)
+
+        return func(*args, **kwargs)
+    return with_ip_check
+
+
+def circleci_new_build(source_event, payload):
+    app.logger.info({
+        'status': 'triggered',
+        'action': 'circleci_new_build',
+        'source_event': source_event,
+        'payload': payload
+    })
+
+    worker_token = os.environ.get('CIRCLECI_TOKEN', None)
+    if not worker_token:
+        app.logger.error('CIRCLECI_TOKEN is not set')
+        return
+    worker_repo = os.environ.get('CIRCLECI_REPO', None)
+    if not worker_repo:
+        app.logger.error('CIRCLECI_REPO is not set')
+        return
+
+    api_url = 'https://circleci.com/api/v1.1/project/github/' + worker_repo
+
+    response = requests.post(api_url, params={'circle-token': worker_token}, json={
+        'build_parameters': {
+            'SLICER_REPO_NAME': payload['repo'],
+            'SLICER_REPO_BRANCH': payload['branch'],
+            'SLICER_REPO_TAG': payload['tag'],
+            'SLICER_REPO_REVISION': payload['revision'],
+        }
+    })
+    response.raise_for_status()
+
+
+@app.route('/')
+@ip_check
+def hello():
+    # Todo: List available endpoints and supported GitHub events
+    return 'Hello World!'
+
+
+@webhook.hook(event_type='ping')
+@ip_check
+def on_ping(data):
+    return 'The impossible exists only until we find a way to make it possible'
+
+
+@webhook.hook(event_type='pull_request')
+@ip_check
+def on_pull_request(data):
+
+    payload = {
+        'repo': data['pull_request']['head']['repo']['full_name'],
+        'branch': data['pull_request']['head']['ref'],
+        'tag': None,
+        'revision': data['pull_request']['head']['sha']
+    }
+
+    source_event = {'type': 'pull_request', 'action': data['action'], 'number': data['number']}
+
+    if data['action'] not in ['opened', 'edited']:
+        app.logger.info({
+            'status': 'ignored',
+            'action': 'circleci_new_build',
+            'source_event': source_event,
+            'payload': payload
+        })
+        return
+
+    circleci_new_build(source_event, payload)
+
+
+@webhook.hook(event_type='push')
+@ip_check
+def on_push(data):
+
+    # Extract repo, branch and tag
+    repo = data['repository']['full_name']
+    branch = None
+    tag = None
+    if data['ref'].startswith('refs/tags/'):
+        tag = data['ref'].lstrip('refs/tags/')
+        branch = data['base_ref'].lstrip('refs/heads/')
+    elif data['ref'].startswith('refs/heads/'):
+        branch = data['ref'].lstrip('refs/heads/')
+    else:
+        app.logger.error('Unsupported ref: %s' % data['ref'])
+        abort(500)
+
+    source_event = {'type': 'push'}
+    payload = {'repo': repo, 'branch': branch, 'tag': tag, 'revision': data['after']}
+
+    # Only consider Push event associated with update to 'master' branch
+    # or tags.
+    if branch != 'master' and not tag:
+        app.logger.info({
+            'status': 'ignored',
+            'source_event': {'type': 'push'},
+            'action': source_event,
+            'payload': payload
+        })
+        return
+
+    circleci_new_build(source_event, payload)
+
+
+if __name__ == '__main__':
+    app.run()
